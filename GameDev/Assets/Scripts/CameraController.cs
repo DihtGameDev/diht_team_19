563c9478910d0d3314122acbcc4e99b981fc60fe
Unrealf1 <fyodor.bukreev@yandex.ru>
2020-04-23 01:22:15 +0300

    Created target system for predators. Now they are able to wander around and eat

---
+++ GameDev/Assets/Scripts/CameraController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    private string upInputAxis = "Vertical";
+    private string sideInputAxis = "Horizontal";
+
+    public float moveSpeed = 20;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        float moveAxis = Input.GetAxis(upInputAxis);
+        float turnAxis = Input.GetAxis(sideInputAxis);
+
+        ApplyMovement(moveAxis, turnAxis);
+    }
+
+    void ApplyMovement(float moveVertical, float moveHorizontal)
+    {
+        transform.Translate(Vector3.up * moveVertical * moveSpeed * Time.deltaTime);
+        transform.Translate(Vector3.right * moveHorizontal * moveSpeed * Time.deltaTime);
+    }
+}
--- GameDev/Assets/Scripts/FoxController.cs
+++ GameDev/Assets/Scripts/FoxController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class FoxController : MonoBehaviour
 {
@@ -15,16 +16,21 @@ public class FoxController : MonoBehaviour
     }
 
     public GameController controller;
-    public float moveSpeed = 5;
+    public float moveSpeed = 1;
     public float satiety = 50;
     public float hunger_rate = 1;
     public float starvation_threshold = 0;
     public float overeating_threshold = 110;
     public State state = State.Calm;
+    public float target_search_delay = 0.5f;
+    public GameObject target_marker_origin = null;
     
     void Start()
     {
         controller.Register(this);
+        target_marker = target_marker_origin != null ? Instantiate(target_marker_origin) : null;
+        target = transform.position;
+        StartCoroutine("SearchForTarget");
     }
     
     void Update()
@@ -41,6 +47,61 @@ public class FoxController : MonoBehaviour
         Vector3 direction = GetDirection();
         var speed = state != State.Overate ? moveSpeed : moveSpeed * 0.7; 
         transform.Translate(direction * moveSpeed * Time.deltaTime);
+    }
+
+    Vector3 GetDirection()
+    {
+        return (target - this.transform.position).normalized;
+    }
+
+    IEnumerator SearchForTarget()
+    {
+        while (true)
+        {
+            UpdateTarget();
+            yield return new WaitForSeconds(target_search_delay);
+        }
+    }
+
+    void UpdateTarget()
+    {
+        var old = target;
+        switch (state)
+        {
+            case State.Overate:
+            case State.Calm:
+                if ((target - transform.position).magnitude < 2)
+                {
+                    target = ChooseRandomTargetNear(transform.position, 20);
+                }
+                break;
+            case State.Hungry:
+                var position = this.transform.position;
+                var food_source = GetClosestFoodSource();
+                target = food_source.transform.position;
+                break;
+            case State.Frenzy:
+                ChooseRandomTargetNear(this.transform.position, 20);
+                break;
+            default:
+                break;
+
+        }
+        if (target_marker != null)
+        {
+            target_marker.transform.SetPositionAndRotation(target + Vector3.up * 5, Quaternion.identity);
+        }
+        string msg = "Updated target from " + old + " to " + target + " in state " + state;
+        Debug.Log(msg);
+    }
+
+    Vector3 ChooseRandomTargetNear(Vector3 pivot, float maxDistance)
+    {
+        Vector3 random_target = Vector3.zero;
+        float angle = UnityEngine.Random.Range(0, (float)Math.PI * 2);
+        float magnitude = UnityEngine.Random.Range(0.01f, maxDistance);
+        random_target = new Vector3(magnitude * (float)Math.Sin(angle), 0, magnitude * (float)Math.Cos(angle));
+        return random_target + pivot;
     }
 
     void ChangeState()
@@ -82,35 +143,10 @@ public class FoxController : MonoBehaviour
         }
     }
 
-    Vector3 GetDirection()
-    {
-        Vector3 direction = Vector3.zero;
-        switch (state)
-        {
-            case State.Calm:
-                direction.x = Random.Range(200, 700);
-                direction.z = Random.Range(200, 700);
-                direction = direction.normalized;
-                break;
-            case State.Hungry:
-                var position = this.transform.position;
-                var food_source = GetClosestFoodSource();
-                var food_position = food_source.transform.position;
-                direction = (food_position - position).normalized;
-                break;
-            case State.Afraid:
-                break;
-            case State.Frenzy:
-                break;
-            default:
-                break;
-        }
-        return direction;
-    }
-
     void Die()
     {
         Debug.Log("The fox is dead :(");
+        StopAllCoroutines();
         Destroy(this);
     }
 
@@ -134,6 +170,7 @@ public class FoxController : MonoBehaviour
             satiety += food_source.value * Time.deltaTime;
         }
     }
-
+    Vector3 target = Vector3.up;
     FoodSourceBehaviour closestFoodSource;
+    GameObject target_marker = null;
 }
